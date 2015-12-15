f06e3fb136cf4e902278b98ed31e0164c8f9dc13
Nathanael Merrill <nathanael@webconnex.com>
2015-12-15 11:50:51 -0800

    adding a specific production config file

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,38 +5,14 @@ var webpack = require('webpack');
 module.exports = {
 	entry: [
 			'babel-polyfill',
-			'webpack/hot/dev-server',
-			'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'app/index.js')
+	    path.resolve(__dirname, 'app/index.js')
     ],
 	output: {
 		path: path.resolve(__dirname, 'public/assets'),
 		publicPath: '/assets/',
 		filename: 'bundle.js'
 	},
-	devServer: {
-	contentBase: 'public',
-	//
-	historyApiFallback: true,
-  hot: true,
-  inline: true,
-  progress: true,
-		watchOptions: {
-	  	aggregateTimeout: 300,
-	  	poll: 1000
-	  },
-	// 	filename: "bundle.js",
-  //   // display only errors to reduce the amount of output
-    stats: { colors: true },
-	//
-  //   // parse host and port from env so this is easy
-  //   // to customize
-    host: process.env.HOST,
-    port: process.env.PORT
-  },
-  plugins: [
-    // new webpack.HotModuleReplacementPlugin()
-	],
+	plugins: [],
 	resolve: {
 		alias: {
 			config: path.resolve('./app/config'),
