e09b9f40d5168ae56e8ae013c55d977c42bd1410
Nathanael Merrill <nathanael@webconnex.com>
2016-02-29 15:12:26 -0800

    Updating details page.

--- app/components/hub-selection/details/meta.js
+++ app/components/hub-selection/details/meta.js
@@ -7,8 +7,8 @@ export default class extends Component {
 		return (
 				<div className="meta">
 					<img className="product-image" src={require('../../../images/hub-fpo.png')} alt="Hub FPO" width="100" height="100"/>
-					FF Front Steer Axle Aluminum PreSet Plus Hub Assembly<br />
-				<em>#{result.HubAssemblyNumber}</em>
+					{result.Description}<br />
+				<em>#{result.PartNumber}</em>
 				</div>
 			)
 	}
