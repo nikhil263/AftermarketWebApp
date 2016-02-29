6c3bf7ba307a36489dbe811a0147854974320902
Nathanael Merrill <nathanael@webconnex.com>
2016-02-29 11:00:33 -0800

    Fixes #18

--- app/components/splash.js
+++ app/components/splash.js
@@ -1,18 +1,23 @@
 import React, { PropTypes, Component } from 'react';
 import { Router, Route, Link, IndexRoute } from 'react-router';
 import Spinner from './global/spinner'
+import { connect } from 'react-redux'
+import { pushPath } from 'redux-simple-router'
 
-export default class Splash extends Component {
+class Splash extends Component {
 	constructor(props) {
 		super(props)
 		this.state = { history: props.history }
 	}
+	handleClick(path) {
+		dispatch(pushPath(path))
+	}
 
 	componentDidMount() {
-		setTimeout(() => {
-			this.state.history.push('/hub-selection');
-			this.state.history.go();
-		}, 1500);
+		// setTimeout(() => {
+		// 	this.state.history.push('/hub-selection');
+		// 	this.state.history.go();
+		// }, 1500);
 	}
 
 	render() {
@@ -23,8 +28,15 @@ export default class Splash extends Component {
 				<h3>Welcome to the</h3>
 				<h1 className="text-center">ConMet<br />Aftermarket</h1>
 				<h3>Service Parts & Replacement Hubs</h3>
-				<Spinner isFetching={true}/>
+				<div className="disclaimer">
+				<p>ConMet is the leading supplier of wheel end technology for commercial vehicles in North America. This app is intended to direct the distributor and/or end-user to a readily available aftermarket replacement component or wheel end assembly.</p>
 
+				<p>Due to the variety of vehicle applications and part numbers we supply to the original equipment manufacturers, this app is not all inclusive. In some cases this app will not have a reference to a direct aftermarket replacement part number. In that instance, we will direct you to our customer service team who can assist with finding the correct service part or wheel end assembly required to get the vehicle back in service.</p>
+
+				<p>CAUTION: As you are guided through the app, it is important that you verify your responses. Failure to do so could result in the wrong service part or hub assembly recommendation. Wheel hubs are a safety critical component and installing the wrong component or assembly on a vehicle can result in premature failure.</p>
+				<Link to="/hub-selection" className="general-button">I Accept</Link>
+
+				</div>
 				</div>
 			</div>
 			<div className="grid-block align-center"></div>
@@ -36,3 +48,4 @@ export default class Splash extends Component {
 		)
 	}
 };
+export default connect()(Splash)
