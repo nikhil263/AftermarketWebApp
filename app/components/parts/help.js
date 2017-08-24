472b06b1e2a389292d05fa8488518e2f6f6d8c4f
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2017-08-24 13:43:22 +0530

    1.Task 1141 reverted.

--- app/components/parts/help.js
+++ app/components/parts/help.js
@@ -10,20 +10,26 @@ class Help extends Component {
 		dispatch(pushPath(path))
 	}
 
+    handleLink(url) {
+        window.location = url;
+    }
+
 	render() {
 		return (
 			<div className="grid-container main-content">
 				<h1>Do you know your hub assembly number?</h1>
 
 				<iframe width="100%" height="300px" src="https://www.youtube.com/embed/9yVp9V2JuEs" frameborder="0" allowfullscreen></iframe>
-					<div className="conmet-button">
-						<button onClick={this.handleClick.bind(this,'/parts/help/detail')} className="yes-no-button"><em>Read</em> about finding your assembly number.</button>
-					</div>
 
-				<div className="conmet-button">
-					<button onClick={this.handleClick.bind(this,'/parts/search')} className="yes-no-button"><em>Yes</em>I know my number now.</button>
+				<div className="conmet-button" >
+					<button onClick={this.handleClick.bind(this, '/hub-selection/search')} className="yes-no-button">
+						<em>Yes</em>I know the hub assembly number now.
+					</button>
 				</div>
 
+				<div className="conmet-button">
+					<button onClick={this.handleLink.bind(this,' http://www.conmet.com/legal/contact-information/')} className="yes-no-button"><em>Contact</em>Customer service for support.</button>
+				</div>
 
 			</div>
 		)
