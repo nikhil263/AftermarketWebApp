4e0b4f31187cdf856fdf678548b866dbe3e37f2a
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2017-09-21 19:03:03 +0530

    Task #1148: 1.Bug Fixes 2.GAWR selection skiped for necessary cases
    Task #1197:
    Display Conventional and PreSet Hub results on one screen

---
+++ app/components/hub-selection/filters.js
@@ -0,0 +1,244 @@
+import React, { PropTypes, Component } from 'react';
+import {fetchFilterValues,fetchHubsCrossApi,fetchHubs} from 'actions';
+import {fetchCategories} from 'actions/categories';
+import { pushPath } from 'redux-simple-router';
+import { connect } from 'react-redux';
+import Spinner from '../global/spinner';
+import { IMAGE_CDN } from 'config/constants';
+import {Link} from 'react-router';
+
+class HubResults extends React.Component {
+    render() {
+        let assemblyType = this.props.assemblyType;
+        if(assemblyType){
+            return (
+                <div className="details">
+                    <div>Best Value:</div>
+                    <ul>
+                        <li>Extended Warranty</li>
+                        <li>Less DownTime</li>
+                        <li>Simplified Process</li>
+                        <li>Liability Protection</li>
+                    </ul>
+                </div>
+            )
+        }else{
+            return (
+                <div className="details">
+                    <div>Not Included:</div>
+                    <ul>
+                        <li>Bearing Cones</li>
+                        <li>Seal</li>
+                        <li>Manually Adjusted</li>
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
+
+class Filters extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filters: [],
+            filter_name: [],
+            current_filter: null,
+            results: [],
+            isFetching: false,
+            url: ''
+        };
+    }
+
+    componentWillMount(){
+        let filters = this.props.params.filters;
+        this.props.dispatch(fetchCategories()).then(()=>{
+            if(filters){
+                this.setFilters(filters);
+            }
+        });
+    }
+
+    componentWillReceiveProps(newProps){
+        if(this.state.url && this.state.url !== newProps.params.filters){
+            this.setFilters(newProps.params.filters);
+        }
+    }
+
+    setFilters(filters){
+        let result = {};
+        filters.split('&').forEach(function(x){
+            let arr = x.split('=');
+            arr[1] && (result[arr[0]] = arr[1]);
+        });
+        this.state.filters = result;
+        this.state.url = filters;
+
+        let categories = this.props.app.categories;
+        let j = 0;
+
+        Object.keys(categories).forEach((key)=>{
+            if(result.hasOwnProperty(categories[key].QueryParameterName) === false && categories[key].QueryParameterName !== 'tmake'){
+                this.state.filter_name[j] = categories[key].QueryParameterName;
+                j++;
+            }
+        });
+
+        this.getFilterValues();
+    }
+
+    getFilterValues(){
+        const { dispatch } = this.props;
+
+        let hanum = this.state.filters.hanum.split(',');
+        let _self = this;
+        let checkSamefilter = true;
+        if(this.state.current_filter !== undefined){
+            Object.keys(this.state.filter_name).forEach((key)=>{
+                if(_self.state.filter_name[key] === this.state.current_filter){
+                    checkSamefilter = false;
+                }
+            });
+        }
+
+        if((this.state.current_filter === 'hatyp' && checkSamefilter)
+            || (!this.state.current_filter && this.state.filter_name[0] === 'hatyp' && hanum.length > 2)
+            || this.state.filter_name[0] === undefined
+            || (this.state.current_filter !== 'hatyp' && this.state.filter_name[0] === 'hatyp' && this.state.current_filter !== null)){
+            this.hubCrossApi();
+        }else{
+            dispatch(fetchFilterValues(this.state.filter_name[0],this.state.url)).then(()=>{
+                let results = this.props.results.filter_value.Results;
+                this.setState({current_filter: this.state.filter_name[0], isFetching: false});
+                this.state.filter_name.shift();
+                this.setState({results: []});
+
+                if(this.state.current_filter === 'gawrr'){
+                    if(results.length === 2){
+                        if(!(results[0].MaxGawrPound >= 13000 && results[0].MaxGawrPound <= 14600 && results[1].MaxGawrPound >= 14600)){
+                            this.state.filter_name[this.state.current_filter] = results[0].Id;
+                            this.getFilterValues();
+                        }
+                    }
+                }
+
+                if(results.length === 1){
+                    this.state.filter_name[this.state.current_filter] = results[0].Id;
+                    this.getFilterValues();
+                }
+            });
+        }
+    }
+
+    handleFilterClick(e){
+        e.preventDefault();
+        this.state.isFetching = true;
+        let id = e.target.id;
+        if(id){
+            let url = this.state.url;
+            url+= '&'+this.state.current_filter+'='+id;
+            this.props.dispatch(pushPath('/hub-selection/filters/'+url));
+        }
+    }
+
+    hubCrossApi(){
+        let url = this.state.url;
+        url+='&inclv=1';
+        this.props.dispatch(fetchHubsCrossApi(url)).then(()=>{
+            this.setState({results: this.props.results.item.Results});
+        });
+        this.props.dispatch(fetchHubs("10082207,10083787")).then(()=>{
+            this.setState({results: this.props.results.item.Results});
+        });
+    }
+
+    render() {
+        let current_filter = this.state.current_filter;
+        let isFetching = this.state.isFetching;
+        let filter_value = this.props.results.filter_value.Results;
+        let results = this.state.results;
+
+        if(current_filter === 'gawrr' && !isFetching && !(results.length > 0)) {
+            return (
+                <div className="grid-container main-content">
+                    <h1>Choose the GAWR (Gross Axle Weight Rating)</h1>
+                    <div className="grid-content">
+                        {filter_value.map((key) => {
+                            return <div className="small-12">
+                                <div className="conmet-button">
+                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
+                                        {key.DisplayRange}
+                                    </button>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            )
+        }else if(current_filter === 'wmslc' && !isFetching && !(results.length > 0)){
+            return (
+                <div className="grid-container main-content">
+                    <h1>Choose the wheel type (determine wheel stud length)</h1>
+                    <div className="grid-content">
+                        {filter_value.map((key) => {
+                            return <div className="small-12">
+                                <div className="conmet-button">
+                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
+                                        {key.WheelMaterial} - {key.StudLengthClass}
+                                    </button>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            )
+        }else if((current_filter === 'hcmty' || current_filter === 'hatyp') && !isFetching && !(results.length > 0)){
+            return (
+                <div className="grid-container main-content">
+                    <h1>{current_filter === 'hcmty' ? 'Choose the hub material' : 'Choose the hub assembly type'}</h1>
+                    <div className="grid-content">
+                        {filter_value.map((key) => {
+                            return <div className="small-12">
+                                <div className="conmet-button">
+                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
+                                        {key.Name}
+                                    </button>
+                                </div>
+                            </div>
+                        })}
+                    </div>
+                </div>
+            )
+        }else if(results.length > 0) {
+            return(
+                <div className="grid-container main-content" id="hubAssemblyResult">
+                    <h1>Success !</h1>
+                    <div className="grid-content flex-row">
+                        {this.state.results.map((item,index) => {
+                            let assemblyType = item.AftermarketDescription.toLowerCase().includes('preset');
+                            return <div className={results.length === 2 ? "small-6" : "small-12"}>
+                                <img className="product-image"  src={IMAGE_CDN+item.Images[0].ImageGuid+'.png'} alt={item.HubAssemblyNumber} />
+                                <div className="type">{assemblyType ? "PreSet" : "Conventional"}</div>
+                                <div className="number">#{item.HubAssemblyNumber}</div>
+                                <HubResults assemblyType={assemblyType} key={index} />
+                                <Link to={'/hub-selection/details/'+item.HubAssemblyNumber} className="general-button">See Details</Link>
+                            </div>
+                        })}
+                        <div className="clearfix" />
+                    </div>
+                    <div className="disclaimer">ConMet Wheel End Disclaimer</div>
+                    <div className="note">
+                        {results[0].GawrNote.Text}
+                    </div>
+                </div>
+            )
+        }else{
+            return (
+                <Spinner isFetching={true} />
+            )
+        }
+    }
+};
+
+export default connect()(Filters);
--- app/components/hub-selection/search.js
+++ app/components/hub-selection/search.js
@@ -1,8 +1,7 @@
 import React, { PropTypes, Component } from 'react';
-import HubSelection from 'components/hub-selection'
-import {connect} from 'react-redux'
-import {fetchHubAssemblyFilters, fetchHubAssemblyNumber, fetchFilterValues,fetchHubsCrossApi} from 'actions';
-import { pushPath } from 'redux-simple-router'
+import {connect} from 'react-redux';
+import {fetchHubAssemblyFilters, fetchHubAssemblyNumber} from 'actions';
+import { pushPath } from 'redux-simple-router';
 import Autocomplete from 'react-autocomplete';
 
 class Results extends Component {
@@ -13,8 +12,6 @@ class Results extends Component {
             value: '',
             assemblyNumber: [],
             loading: false,
-            filter_name: [],
-            current_filter: null,
             url: ''
         };
     }
@@ -29,174 +26,69 @@ class Results extends Component {
             const { dispatch } = this.props;
             dispatch(fetchHubAssemblyFilters(id)).then(()=>{
                 let filters = this.props.results.filters.Results;
-                let categories = this.props.app.categories;
-                let j = 0;
+                let i = 1, length = Object.keys(filters).length;
 
-                Object.keys(categories).forEach((key)=>{
-                    if(filters.hasOwnProperty(categories[key].QueryParameterName) === false && categories[key].QueryParameterName !== 'tmake'){
-                        this.state.filter_name[j] = categories[key].QueryParameterName;
-                        j++;
-                    }
+                Object.keys(filters).forEach((key)=>{
+                    this.state.url += (i === length) ? key+'='+filters[key] : key+'='+filters[key]+"&";
+                    i++;
                 });
 
-                this.getFilterValues();
+                this.props.dispatch(pushPath('/hub-selection/filters/'+this.state.url));
             });
-        }
-    }
-
-    getFilterValues(id = null){
-        const { dispatch } = this.props;
-        let filters = this.props.results.filters.Results;
-
-        if(this.state.url === ''){
-            let i = 1, length = Object.keys(filters).length;
-            Object.keys(filters).forEach((key)=>{
-                this.state.url += (i === length) ? key+'='+filters[key] : key+'='+filters[key]+"&";
-                i++;
-            });
-        }
-
-        if(this.state.current_filter !== null && id !== null){
-            this.state.url+= '&'+this.state.current_filter+'='+id;
-            this.state.filter_name[this.state.current_filter] = id;
-        }
-
-        if(this.state.current_filter === 'hatyp' || this.state.filter_name[0] === undefined || (this.state.current_filter !== 'hatyp' && this.state.filter_name[0] === 'hatyp' && this.state.current_filter !== null)){
-            this.hubCrossApi();
-        }else{
-            dispatch(fetchFilterValues(this.state.filter_name[0],this.state.url)).then(()=>{
-                let results = this.props.results.filter_value.Results;
-                this.setState({current_filter: this.state.filter_name[0]});
-                this.state.filter_name.shift();
-                if(results.length === 1){
-                    this.getFilterValues(results[0].Id);
-                }
-            });
-        }
-    }
-
-    hubCrossApi(){
-        this.state.url += '&inclv=1';
-        this.props.dispatch(fetchHubsCrossApi(this.state.url)).then(()=>{
-            this.props.dispatch(pushPath('/hub-selection/search/'+this.props.results.item.Results[0].HubAssemblyNumber));
-        });
-    }
-
-    handleFilterClick(e){
-        e.preventDefault();
-        let id = e.target.id;
-        if(id){
-            this.getFilterValues(e.target.id)
         }
     }
 
     render() {
-        let current_filter = this.state.current_filter;
-        let results = this.props.results.filter_value.Results;
-
-        if(current_filter === 'gawrr') {
-            return (
-                <div className="grid-container main-content">
-                    <h1>Choose the GAWR (Gross Axle Weight Rating)</h1>
-                    <div className="grid-content">
-                        {results.map((key) => {
-                            return <div className="small-12">
-                                <div className="conmet-button">
-                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
-                                        {key.DisplayRange}
-                                    </button>
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                </div>
-            )
-        }else if(current_filter === 'wmslc'){
-            return (
-                <div className="grid-container main-content">
-                    <h1>Choose the wheel type (determine wheel stud length)</h1>
-                    <div className="grid-content">
-                        {results.map((key) => {
-                            return <div className="small-12">
-                                <div className="conmet-button">
-                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
-                                        {key.WheelMaterial} - {key.StudLengthClass}
-                                    </button>
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                </div>
-            )
-        }else if(current_filter === 'hcmty' || current_filter === 'hatyp'){
-            return (
-                <div className="grid-container main-content">
-                    <h1>{current_filter === 'hcmty' ? 'Choose the hub material' : 'Choose the hub assembly type'}</h1>
-                    <div className="grid-content">
-                        {results.map((key) => {
-                            return <div className="small-12">
-                                <div className="conmet-button">
-                                    <button className="yes-no-button bold" id={key.Id} key={key.Id} onClick={this.handleFilterClick.bind(this)}>
-                                        {key.Name}
-                                    </button>
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                </div>
-            )
-        }else{
-            return (
-                <div className="grid-container main-content">
-                    <h1>Enter your hub assembly number</h1>
-                    <form id="autoComplete">
-                        <Autocomplete
-                            value={this.state.value}
-                            inputProps={{ placeholder: 'Please enter a valid ConMet or Competitor assembly number',className: 'assembly-number', id:'assemblyNumber',type:'text' }}
-                            wrapperStyle={{ position: 'relative',display: 'block', margin: '0 0 1rem 0' }}
-                            items={this.state.assemblyNumber}
-                            getItemValue={(item) => item.Competitor+' '+item.HubAssemblyNumber}
-                            onSelect={(value, state) => {
-                                this.props.results.selectedHubAssemblyNumber = state.Competitor+' '+state.HubAssemblyNumber;
-                                this.hubAssemblyFilters(state.CompetitorHubAssemblyNumberId);
-                                this.setState({ value, assemblyNumber: [state] })
-                            }}
-                            onChange={(event, value) => {
-                                this.setState({value, assemblyNumber : [], loading: true });
-                                if(value !== ''){
-                                    this.props.dispatch(fetchHubAssemblyNumber(value)).then(() => {
-                                        let results = this.props.results.assemblyNumber.Results;
-                                        if(results){
-                                            this.setState({ assemblyNumber: this.props.results.assemblyNumber.Results, loading: false });
-                                        }else{
-                                            this.setState({ assemblyNumber: [], loading: false });
-                                        }
-                                    });
-                                }
-                            }}
-                            renderItem={(item, isHighlighted) =>
-                                <div className="menu-item" style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.CompetitorHubAssemblyNumberId}>
-                                    {item.Competitor}  {item.HubAssemblyNumber}
-                                </div>
+        return (
+            <div className="grid-container main-content">
+                <h1>Enter your hub assembly number</h1>
+                <form id="autoComplete">
+                    <Autocomplete
+                        value={this.state.value}
+                        inputProps={{ placeholder: 'Please enter a valid ConMet or Competitor assembly number',className: 'assembly-number', id:'assemblyNumber',type:'text' }}
+                        wrapperStyle={{ position: 'relative',display: 'block', margin: '0 0 1rem 0' }}
+                        items={this.state.assemblyNumber}
+                        getItemValue={(item) => item.Competitor+' '+item.HubAssemblyNumber}
+                        onSelect={(value, state) => {
+                            this.props.results.selectedHubAssemblyNumber = state.Competitor+' '+state.HubAssemblyNumber;
+                            this.hubAssemblyFilters(state.CompetitorHubAssemblyNumberId);
+                            this.setState({ value, assemblyNumber: [state] })
+                        }}
+                        onChange={(event, value) => {
+                            this.setState({value, assemblyNumber : [], loading: true });
+                            if(value !== ''){
+                                this.props.dispatch(fetchHubAssemblyNumber(value)).then(() => {
+                                    let results = this.props.results.assemblyNumber.Results;
+                                    if(results){
+                                        this.setState({ assemblyNumber: this.props.results.assemblyNumber.Results, loading: false });
+                                    }else{
+                                        this.setState({ assemblyNumber: [], loading: false });
+                                    }
+                                });
                             }
-                            renderMenu={(items, value) => (
-                                <div className="menu">
-                                    {value === '' ? (
-                                        <div className="item">Type ConMet or Competitor assembly number</div>
-                                    ) : this.state.loading ? (
-                                        <div className="item">Loading...</div>
-                                    ) : items.length === 0 ? (
-                                        <div className="item">No matches for {value}</div>
-                                    ) : items}
-                                </div>
-                            )}
-                        />
-                        <div className="help">Some examples: “104444”, “ConMet 10031065”, “Gunite 5669‑1”, “Webb 20231‑‑1T‑71”</div>
-                        <button type="submit" className="button general-button" onClick={this.doSearch.bind(this)}>Continue</button>
-                    </form>
-                </div>
-            )
-        }
+                        }}
+                        renderItem={(item, isHighlighted) =>
+                            <div className="menu-item" style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.CompetitorHubAssemblyNumberId}>
+                                {item.Competitor}  {item.HubAssemblyNumber}
+                            </div>
+                        }
+                        renderMenu={(items, value) => (
+                            <div className="menu">
+                                {value === '' ? (
+                                    <div className="item">Type ConMet or Competitor assembly number</div>
+                                ) : this.state.loading ? (
+                                    <div className="item">Loading...</div>
+                                ) : items.length === 0 ? (
+                                    <div className="item">No matches for {value}</div>
+                                ) : items}
+                            </div>
+                        )}
+                    />
+                    <div className="help">Some examples: “104444”, “ConMet 10031065”, “Gunite 5669‑1”, “Webb 20231‑‑1T‑71”</div>
+                    <button type="submit" className="button general-button" onClick={this.doSearch.bind(this)}>Continue</button>
+                </form>
+            </div>
+        )
     }
 };
 
