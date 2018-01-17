bbaa90e0d7ce92012a24e94e2bb0625859b8be68
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-01-17 20:06:25 +0530

    Task #1246: Removed stud links

--- app/components/parts/choose-path.js
+++ app/components/parts/choose-path.js
@@ -11,7 +11,7 @@ class PartsChoosePath extends React.Component {
                 <Link to="/parts/search" className="general-button">Hub Components by Assembly</Link>
                 <a href="https://conmet.com/wp-content/uploads/2016/11/Bearing-Cross-Reference-7-2015-1.pdf" target="_blank" className="general-button">Bearing Cross Reference</a>
                 <a href="https://conmet.com/wp-content/uploads/2016/11/Seal-Cross-Reference-3-2016-1.pdf" target="_blank" className="general-button">Seal Cross Reference</a>
-                <a href="https://hubcatalog.conmet.com/StudChart.aspx" target="_blank" className="general-button">Stud Chart</a>
+                {/*<a href="https://hubcatalog.conmet.com/StudChart.aspx" target="_blank" className="general-button">Stud Chart</a>*/}
             </div>
         );
     }
