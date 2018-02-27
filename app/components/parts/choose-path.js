c3fea5a76ba4260129c967ea2f09ef8017553be3
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-02-27 12:13:50 +0530

    Task #1246

--- app/components/parts/choose-path.js
+++ app/components/parts/choose-path.js
@@ -9,8 +9,8 @@ class PartsChoosePath extends React.Component {
                 <h1>What are you looking for?</h1>
 
                 <Link to="/parts/search" className="general-button">Hub Components by Assembly</Link>
-                <a href="https://conmet.com/wp-content/uploads/2016/11/Bearing-Cross-Reference-7-2015-1.pdf" target="_blank" className="general-button">Bearing Cross Reference</a>
-                <a href="https://conmet.com/wp-content/uploads/2016/11/Seal-Cross-Reference-3-2016-1.pdf" target="_blank" className="general-button">Seal Cross Reference</a>
+                <a href="https://www.conmet.com/wp-content/uploads/2018/02/Wheel-Bearing-Cross-Reference.pdf" target="_blank" className="general-button">Bearing Cross Reference</a>
+                <a href="https://www.conmet.com/wp-content/uploads/2018/02/ConMet-Wheel-Seal-Cross-Reference.pdf" target="_blank" className="general-button">Seal Cross Reference</a>
                 {/*<a href="https://hubcatalog.conmet.com/StudChart.aspx" target="_blank" className="general-button">Stud Chart</a>*/}
             </div>
         );
