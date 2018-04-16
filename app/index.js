a8f11498f6ececd8ff4789ecf71b97a5f84d6a9f
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-04-16 13:49:13 +0530

    Google Analytics Integration and trigger page view event on route change

--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import React, { PropTypes, Component } from 'react';
 import {render} from 'react-dom'
-import { Provider } from 'react-redux';
 import {createHistory } from 'history';
 import { syncReduxAndRouter } from 'redux-simple-router'
 import configureStore from 'config/store';
-
+const GA_TRACKING_ID = 'UA-117509246-1';
 const store = configureStore();
 const history = createHistory();
 
-syncReduxAndRouter(history, store)
+syncReduxAndRouter(history, store);
+const historyChangeHandler = function (history) {
+    history.listen(function(location) {
+        gtag('config', GA_TRACKING_ID, {
+            'page_path': location.pathname
+        });
+    });
+};
+
+historyChangeHandler(history);
 
 import Root from 'containers/root'
 
@@ -18,4 +26,4 @@ if (Function('/*@cc_on return document.documentMode===10@*/')()){
     document.documentElement.className+=' ie10';
 }
 
-render(<Root store={store} history={history} />, document.getElementById('app'))
+render(<Root store={store} history={history} />, document.getElementById('app'));
