411c552d9ed394218d513cff6724795d8cc64f64
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-04-19 16:53:20 +0530

    Task #1290 - Removed duplicate text on Component Hub Assembly Entry

--- app/components/parts/search.js
+++ app/components/parts/search.js
@@ -18,7 +18,6 @@ class PartsSearch extends Component {
 			<div className="grid-container main-content">
 				<h1>Enter your hub assembly number</h1>
 				<div className="error">
-				 <p>Please enter a valid 6 or 8 digit assembly number</p>
 				</div>
           <form onSubmit={this.doSearch.bind(this)}>
 					<input id="assemblyNumber" defaultValue="" className="assembly-number" type="text" placeholder="6 or 8 digit assembly number (10031065)" />
