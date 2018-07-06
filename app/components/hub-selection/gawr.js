ab92eed5c13f2d32b134e53d24dcd7420ec4616e
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-07-06 17:05:43 +0530

    Task #1267 -  Add ratings to AL or Iron selection buttons. "up to"

--- app/components/hub-selection/gawr.js
+++ app/components/hub-selection/gawr.js
@@ -1,78 +1,76 @@
 import React, { PropTypes, Component } from 'react';
-import HubSelection from 'components/hub-selection'
-import  { connect } from 'react-redux'
-import { pushPath } from 'redux-simple-router'
-import Spinner from 'components/global/spinner'
-import { setActiveFilterValue, fetchFilters } from 'actions/filters'
+import  { connect } from 'react-redux';
+import Spinner from 'components/global/spinner';
+import { fetchFilters } from 'actions/filters';
 
-const FILTERIDX=6
-const NEXT_FILTER_PATH = '/hub-selection/axle-stud'
+const FILTERIDX = 6;
 
 class Result extends Component {
-	convertToKg() {
-		const { result } = this.props
-		var maxKg, minKg
-		maxKg = Math.round(result.MaxGawrPound * 0.453592).toLocaleString();
-		minKg = Math.round(result.MinGawrPound * 0.453592).toLocaleString();
-		return(
-			<span>({minKg}-{maxKg} kg)</span>
-		)
-	}
-	render() {
-		const {active, onClick, result} = this.props
-		var min, max, display
 
+  convertToKg() {
+    const { result } = this.props;
+    let maxKg, minKg;
+    maxKg = Math.round(result.MaxGawrPound * 0.453592).toLocaleString();
+    minKg = Math.round(result.MinGawrPound * 0.453592).toLocaleString();
 
-			min = result.MinGawrPound.toLocaleString()
-			max = result.MaxGawrPound.toLocaleString()
-			if (min === '0') {
-				display = 'Up to '+max+' lbs.'
-			} else {
-				display = min +' - ' + max+' lbs.'
-			}
+    return(
+      <span>({minKg}-{maxKg} kg)</span>
+    )
+  }
 
+  render() {
+    const {active, onClick, result} = this.props;
+    let min, max, display;
 
+    min = result.MinGawrPound.toLocaleString();
+    max = result.MaxGawrPound.toLocaleString();
 
+    if (min === '0') {
+      display = 'Up to '+max+' lbs.'
+    } else {
+      display = min +' - ' + max+' lbs.'
+    }
 
-		return (
-			<div className={active()}>
-				<button className="yes-no-button" onClick={onClick}><strong>{display}</strong><br />
-					{this.convertToKg()}
-				</button>
-			</div>
-		)
-	}
+    return (
+      <div className={active()}>
+        <button className="yes-no-button" onClick={onClick}><strong>{display}</strong><br />
+          {this.convertToKg()}
+        </button>
+      </div>
+    )
+  }
 }
 
 class GAWR extends Component {
 
-	componentDidMount() {
-		const { dispatch, app } = this.props
-		dispatch(fetchFilters(FILTERIDX, app))
-	}
+  componentDidMount() {
+    const { dispatch, app } = this.props;
+    const { brkty, tmake, aaxna } = app.filterState;
+    const skipGawrFilter = brkty === 1 && tmake === 1 && aaxna === 4;
 
+    dispatch(fetchFilters(skipGawrFilter ? 7 : FILTERIDX, app));
+  }
 
-	render() {
-		const {app,setFilter, setActive} = this.props
+  render() {
+    const {app, setFilter, setActive} = this.props;
 
-		if (app.isFetching || app.filterResults.length < 1) {
-			return <Spinner isFetching={app.isFetching} />
-		}
-		return (
-			<div className="grid-container main-content">
-				<h1>Choose the GAWR <br />(Gross Axle Weight Rating):</h1>
+    if (app.isFetching || app.filterResults.length < 1) {
+      return <Spinner isFetching={app.isFetching} />
+    }
 
-					{app.filterResults.map((result, index) => {
-						var boundClick = setFilter.bind(this, FILTERIDX, {gawrr: result.Id}, app);
-						var boundActive = setActive.bind(this, FILTERIDX, result.Id);
-						return <Result key={index} result={result} onClick={boundClick} active={boundActive} />
-
-					})}
-
-				
-			</div>
-		)
-	}
-};
+    return (
+      <div className="grid-container main-content">
+        <h1>Choose the GAWR <br />(Gross Axle Weight Rating):</h1>
+        {
+          app.filterResults.map((result, index) => {
+            var boundClick = setFilter.bind(this, FILTERIDX, {gawrr: result.Id}, app);
+            var boundActive = setActive.bind(this, FILTERIDX, result.Id);
+            return <Result key={index} result={result} onClick={boundClick} active={boundActive} />
+          })
+        }
+      </div>
+    )
+  }
+}
 
 export default connect()(GAWR)
