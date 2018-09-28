72d965d5a8028e50a29b28b710bcdfe936e23118
Mulraj Gupta <mulraj.gupta@mediatech.co.in>
2018-09-28 14:29:41 +0530

    Task #1364 - Add new type to the Hub components result page

--- app/components/parts/results.js
+++ app/components/parts/results.js
@@ -1,20 +1,17 @@
 import React, { PropTypes, Component } from 'react';
-import {connect} from 'react-redux'
-import { pushPath } from 'redux-simple-router'
-import {fetchParts, fetchRebuildKitDetails} from 'actions/parts'
-import { PARTTYPES, DOUBLE_END_STUDS } from 'config/constants'
-import Part from './part'
-import NoResults from '../global/no-result'
-import Waiting from '../global/waiting'
-import _ from 'lodash'
-import {Link} from 'react-router';
+import { connect } from 'react-redux';
+import { pushPath } from 'redux-simple-router';
+import { fetchParts, fetchRebuildKitDetails } from 'actions/parts';
+import { PARTTYPES, DOUBLE_END_STUDS } from 'config/constants';
+import NoResults from '../global/no-result';
+import Waiting from '../global/waiting';
+import _ from 'lodash';
+import { Link } from 'react-router';
 import classNames from 'classnames';
 
 const FULLREPLACE = [115];
 const SERVICEPARTS = [220, 5, 6, 7, 8, 101];
-// const SERVICEKITS = [217, 218, 219, 221]
-// const SPINDLENUTS = [222, 223, 226, 227]
-const SERVICEKITS = [217, 218, 219, 221, 222, 223, 226, 227,203];
+const SERVICEKITS = [217, 218, 219, 221, 222, 223, 226, 227, 203, 241, 7];
 const SPINDLENUTS = [];
 const SPINDLESOCKETSIZE = { 10036548: 2, 10036549: 2, 10036550: 2.75, 10036551: 3.75, 10036552: 3.125, 10036553: 4 };
 
@@ -27,13 +24,12 @@ class Results extends Component {
     };
   }
   componentDidMount() {
-    const {dispatch, params} = this.props;
+    const { dispatch, params } = this.props;
     if (params.id) {
-      dispatch(fetchParts(params.id))
+      dispatch(fetchParts(params.id));
     } else {
       dispatch(pushPath('/parts/search'));
     }
-
   }
 
   showHeader(allowed = []) {
@@ -42,45 +38,49 @@ class Results extends Component {
     parts.AftermarketParts.map((item, index) => {
       console.log(allowed, item);
       if (-1 !== allowed.indexOf(item.TypeId)) {
-        found  = true;
+        found = true;
       }
-    })
+    });
     return found;
   }
 
   renderTable(viewArray = [], item) {
     const { dispatch, parts } = this.props;
     if (!viewArray) {
-      return null
+      return null;
     }
 
-    let view = viewArray.map((part, index) =>{
+    let view = viewArray.map((part, index) => {
       let appendStr = '';
       if (item.Usages && item.Usages.length > 0) {
-        appendStr = item.Usages[part.UsageId]
+        appendStr = item.Usages[part.UsageId];
       }
 
       if (viewArray.length > 1 && part.TypeId === 115) {
         //if (part.Description.indexOf('PreSet') > -1) {
         return (
           <tr key={index}>
-            <td><Link to={'/hub-selection/details/'+part.PartNumber} >{item.AftermarketPartTypeName+appendStr}</Link></td>
+            <td>
+              <Link to={'/hub-selection/details/' + part.PartNumber}>{item.AftermarketPartTypeName + appendStr}</Link>
+            </td>
             <td>{part.PartNumber}</td>
           </tr>
-        )
+        );
         //}
       } else {
         if (part.TypeId === 115) {
           return (
             <tr key={index}>
-              <td><Link to={'/hub-selection/details/'+part.PartNumber} >{item.AftermarketPartTypeName+appendStr}</Link></td>
+              <td>
+                <Link to={'/hub-selection/details/' + part.PartNumber}>{item.AftermarketPartTypeName + appendStr}</Link>
+              </td>
               <td>{part.PartNumber}</td>
             </tr>
-          )
-        }else {
+          );
+        } else {
           const isRebuildKit = parts.rebuildKitDetails.length && part.TypeId === 219;
-          let partName = item.AftermarketPartTypeName+appendStr;
-          if (part.TypeId === 219 && (parts.rebuildKitNumber !== part.PartNumber)) {
+          let partName = item.AftermarketPartTypeName + appendStr;
+          if (part.TypeId === 219 && parts.rebuildKitNumber !== part.PartNumber) {
             dispatch(fetchRebuildKitDetails(part.PartNumber));
           }
 
@@ -94,26 +94,49 @@ class Results extends Component {
                 <table>
                   <tbody>
                   <tr>
-                    <td>{isRebuildKit ? <span className={classNames('rebuild-kit-toggle', { show: this.state.rebuildKitActive })} onClick={() => this.setState({ rebuildKitActive: !this.state.rebuildKitActive })} /> : null}{partName}</td>
+                    <td>
+                      {isRebuildKit ? (
+                        <span
+                          className={classNames('rebuild-kit-toggle', { show: this.state.rebuildKitActive })}
+                          onClick={() => this.setState({ rebuildKitActive: !this.state.rebuildKitActive })}
+                        />
+                      ) : null}
+                      {partName}
+                    </td>
                     <td className="center">
                       {part.PartNumber}
-                      <div className="help2">{SPINDLESOCKETSIZE[part.PartNumber] ? "("+SPINDLESOCKETSIZE[part.PartNumber]+'" Socket)' : ""}</div>
+                      <div className="help2">
+                        {SPINDLESOCKETSIZE[part.PartNumber]
+                          ? '(' + SPINDLESOCKETSIZE[part.PartNumber] + '" Socket)'
+                          : ''}
+                      </div>
                     </td>
                   </tr>
                   </tbody>
                 </table>
-                {
-                  isRebuildKit ?
-                    parts.rebuildKitDetails.map((item) => {
-                      return (
-                        <div className={classNames('rebuild-kit-info', { show: this.state.rebuildKitActive })} key={item.PreSetPreSetPlusHubRebuildKitNumber}>
-                          <div><span className="text">Inner Bearing</span> <span className="number">{item.BearingSetNumberInboard}</span></div>
-                          <div><span className="text">Outer Bearing</span> <span className="number">{item.BearingSetNumberOutboard}</span></div>
-                          <div><span className="text">Seal & Spacer</span> <span className="number">{item.SealAndSpacerKitNumber}</span></div>
+                {isRebuildKit
+                  ? parts.rebuildKitDetails.map(item => {
+                    return (
+                      <div
+                        className={classNames('rebuild-kit-info', { show: this.state.rebuildKitActive })}
+                        key={item.PreSetPreSetPlusHubRebuildKitNumber}
+                      >
+                        <div>
+                          <span className="text">Inner Bearing</span>{' '}
+                          <span className="number">{item.BearingSetNumberInboard}</span>
                         </div>
-                      );
-                    }) : null
-                }
+                        <div>
+                          <span className="text">Outer Bearing</span>{' '}
+                          <span className="number">{item.BearingSetNumberOutboard}</span>
+                        </div>
+                        <div>
+                          <span className="text">Seal & Spacer</span>{' '}
+                          <span className="number">{item.SealAndSpacerKitNumber}</span>
+                        </div>
+                      </div>
+                    );
+                  })
+                  : null}
               </td>
             </tr>
           );
@@ -121,93 +144,93 @@ class Results extends Component {
       }
     });
 
-    return view
+    return view;
   }
 
   render() {
     const { parts, dispatch, images, app, history } = this.props;
 
     if (parts.isFetching) {
-      return (<Waiting />)
+      return <Waiting />;
     }
 
     if (parts.AftermarketParts.length === 0) {
-      return (<NoResults />)
+      return <NoResults />;
     }
 
-    let replacementHeader, serviceKitHeader, servicePartHeader, spindleNutsHeader = null;
+    let replacementHeader,
+      serviceKitHeader,
+      servicePartHeader,
+      spindleNutsHeader = null;
     parts.AftermarketParts.map((item, index) => {
-
       if (-1 !== FULLREPLACE.indexOf(item.TypeId)) {
-        replacementHeader = <h2>Full Replacement Hub</h2>
+        replacementHeader = <h2>Full Replacement Hub</h2>;
       }
 
       if (-1 !== SERVICEKITS.indexOf(item.TypeId)) {
-        serviceKitHeader = <h2>Service & Rebuild Kits</h2>
+        serviceKitHeader = <h2>Service & Rebuild Kits</h2>;
       }
 
       if (-1 !== SERVICEPARTS.indexOf(item.TypeId)) {
-        servicePartHeader = <h2>Service Parts</h2>
+        servicePartHeader = <h2>Service Parts</h2>;
       }
 
       if (-1 !== SPINDLENUTS.indexOf(item.TypeId)) {
-        spindleNutsHeader = <h2>Spindle Nut Kits</h2>
+        spindleNutsHeader = <h2>Spindle Nut Kits</h2>;
       }
-    })
+    });
+    console.log(parts.AftermarketParts);
     return (
       <div className="grid-container main-content">
         <h2>Hub Components Search Results for:</h2>
-        <h2 className="partsSubHead">{parts.HubAssemblyDescription} <span className="number">{parts.HubAssemblyNumber}</span></h2>
+        <h2 className="partsSubHead">
+          {parts.HubAssemblyDescription} <span className="number">{parts.HubAssemblyNumber}</span>
+        </h2>
         <div className="parts">
           {replacementHeader}
           <table>
             <tbody>
             {PARTTYPES.map((item, index) => {
               if (-1 < FULLREPLACE.indexOf(item.PartTypeId)) {
-                let filtered = _.filter(parts.AftermarketParts, {TypeId: item.PartTypeId})
-                filtered = filtered.sort(function(a,b){
+                let filtered = _.filter(parts.AftermarketParts, { TypeId: item.PartTypeId });
+                filtered = filtered.sort(function(a, b) {
                   return a.Ranking > b.Ranking;
                 });
                 //Return only the first item
-                if(filtered.length) {
-                  return this.renderTable([filtered.shift()], item)
+                if (filtered.length) {
+                  return this.renderTable([filtered.shift()], item);
                 }
               }
-
             })}
             </tbody>
           </table>
 
           {serviceKitHeader}
 
-
           <table>
             <tbody>
             {PARTTYPES.map((item, index) => {
               if (-1 < SERVICEKITS.indexOf(item.PartTypeId)) {
-                let filtered = _.filter(parts.AftermarketParts, {TypeId: item.PartTypeId});
-                if(filtered.length) {
-                  return this.renderTable(filtered, item)
+                let filtered = _.filter(parts.AftermarketParts, { TypeId: item.PartTypeId });
+                if (filtered.length) {
+                  return this.renderTable(filtered, item);
                 }
               }
-
             })}
             </tbody>
           </table>
 
           {servicePartHeader}
 
-
           <table>
             <tbody>
             {PARTTYPES.map((item, index) => {
               if (-1 < SERVICEPARTS.indexOf(item.PartTypeId)) {
-                let filtered = _.filter(parts.AftermarketParts, {TypeId: item.PartTypeId});
-                if(filtered.length) {
-                  return this.renderTable(filtered, item)
+                let filtered = _.filter(parts.AftermarketParts, { TypeId: item.PartTypeId });
+                if (filtered.length) {
+                  return this.renderTable(filtered, item);
                 }
               }
-
             })}
             </tbody>
           </table>
@@ -218,20 +241,20 @@ class Results extends Component {
             <tbody>
             {PARTTYPES.map((item, index) => {
               if (-1 < SPINDLENUTS.indexOf(item.PartTypeId)) {
-                let filtered = _.filter(parts.AftermarketParts, {TypeId: item.PartTypeId});
-                if(filtered.length) {
-                  return this.renderTable(filtered, item)
+                let filtered = _.filter(parts.AftermarketParts, { TypeId: item.PartTypeId });
+                if (filtered.length) {
+                  return this.renderTable(filtered, item);
                 }
               }
-
             })}
             </tbody>
           </table>
         </div>
-        <a href="javascript:void(0)" onClick={history.goBack.bind(this)} className="general-button">Return to search</a>
+        <a href="javascript:void(0)" onClick={history.goBack.bind(this)} className="general-button">
+          Return to search
+        </a>
       </div>
-
-    )
+    );
   }
 }
-export default connect()(Results)
+export default connect()(Results);
