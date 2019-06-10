259f029af65bea9b22eb32e712648f53d4696e33
Kamleshchandra Gupta <kamlesh.gupta@mediatech.co.in>
2019-06-10 12:43:47 +0530

    Task#1443 - Remove Link to hubcatalog.conmet.com

--- app/components/global/no-result.js
+++ app/components/global/no-result.js
@@ -6,20 +6,18 @@ class NoResults extends Component {
 	render(){
 		return (
 			<div className="grid-container main-content">
-				<h1>Sorry. No results were found.</h1>
+				<h1>No results were found.</h1>
 				<div className="not-found">
 				<p>
-					This app is not all inclusive. The part number you entered is not referenced in this database,
+					The part number you entered is not referenced in this database,
 					please verify that you’ve entered the correct assembly number. If you get the same result,
-					please click on the link below to access our more extensive engineering hub catalog or
-					call <a href="tel://1-800-547-9473">1-800-547-9473</a> and speak directly to one of our customer service professionals to identify
-					the replacement hub part  number (service part) you need.
+					please call <a href="tel://1-800-547-9473">1-800-547-9473</a> to speak to a Customer Service Representative.
 				</p>
 
 
 				</div>
 
-				<a href="https://hubcatalog.conmet.com/" className="general-button">ConMet Online Parts Catalog</a>
+				{/*<a href="https://hubcatalog.conmet.com/" className="general-button">ConMet Online Parts Catalog</a>*/}
 
 			</div>
 		)
