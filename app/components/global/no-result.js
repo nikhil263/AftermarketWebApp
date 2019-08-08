b94bc024a01983ed44bcb6ecb645766d5f907276
Kamleshchandra Gupta <kamlesh.gupta@mediatech.co.in>
2019-08-08 15:38:12 +0530

    Task #1591 - Add "No Results Found" Page to Drums/Rotors

--- app/components/global/no-result.js
+++ app/components/global/no-result.js
@@ -10,7 +10,7 @@ class NoResults extends Component {
 				<div className="not-found">
 				<h2 style={{fontSize: '24px',textTransform: 'none'}}>
 					The part number you entered is not referenced in this database,
-					please verify that you’ve entered the correct assembly number. If you get the same result,
+					please verify that you’ve entered the correct part number. If you get the same result,
 					please call <a href="tel://1-800-547-9473">1-800-547-9473</a> to speak to a Customer Service Representative.
 				</h2>
 
